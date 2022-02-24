2dd6b77ba1d4c45a1c7634e3d5bdd2ae822776c9
Denis Paul <paul_denis100@yahoo.com>
2022-02-24 17:31:00 +0200

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import { Switch, Route } from 'react-router-dom';
-import './App.css';
 
 function App() {
   return (
