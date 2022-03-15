20dfb36971dd488155b5e9d3292a14b78942f8f1
Denis Paul <paul_denis100@yahoo.com>
2022-03-15 11:48:50 +0200

    added collection state into redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -6,6 +6,7 @@ import storage from "redux-persist/lib/storage"; // localStorage obj from the wi
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
 // persist config obj
 const persistConfig = {
@@ -17,7 +18,8 @@ const persistConfig = {
 const rootReducer = combineReducers({
     user: userReducer, // handled by Firebase auth
     cart: cartReducer,
-    directory: directoryReducer
+    directory: directoryReducer,
+    shop: shopReducer
 });
 
 export default persistReducer(persistConfig, rootReducer); // returns root reducer with persistence capabilities
